4e6ceb1a6c80bac4df2b3bd0621d251a610d63f4
EliottDup <eliott.duparc@gmail.com>
2024-03-31 11:37:35 +0200

    Version 1.0

---
+++ Assets/Scripts/Ditherer.cs
@@ -0,0 +1,337 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Serialization;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+using System.IO;
+using Random = UnityEngine.Random;
+
+public class Ditherer : MonoBehaviour
+{
+    [SerializeField]
+    ComputeShader shader;
+    RenderTexture result;
+    ComputeBuffer matrixBuffer;
+    ComputeBuffer paletteBuffer;
+
+    float[,] thresholdMatrix;
+    public List<Color> palette;
+
+    public void InitThresholdMatrix(int twoN)
+    {
+        float[,] m = MakeThresholdMatrix(twoN);
+        thresholdMatrix = AddMatrix(m, -0.5f);
+        printMatrix(thresholdMatrix);
+    }
+
+    public List<Color> InitPalette(Texture2D image, int colorCount, int iterationCount)
+    {
+        print("remove duplicate pixels");
+        Color[] pixels = image.GetPixels();
+        HashSet<Color> uniqueColors = new HashSet<Color>();
+        foreach (Color pixelColor in pixels)
+        {
+            if (!uniqueColors.Contains(pixelColor))
+            {
+                uniqueColors.Add(pixelColor);
+            }
+        }
+
+        List<Color> allColors = new List<Color>(uniqueColors);
+
+        print("done removing duplicate pixels");
+        print("making palette");
+
+        List<Color> tmpPaletteColors = GenerateKMeansPalette(allColors, colorCount, iterationCount);
+        List<Color> paletteColors = new List<Color>();
+        for (int i = 0; i < tmpPaletteColors.Count; i++)
+        {
+            if (!paletteColors.Contains(tmpPaletteColors[i]))
+            {
+                paletteColors.Add(tmpPaletteColors[i]);
+            }
+        }
+        print(paletteColors.Count);
+        print("palette made");
+
+        palette = paletteColors;
+        return palette;
+    }
+
+    List<Color> MedianCutColors(List<Color> colors, int n)
+    {
+        if (n == 0)
+        {
+            Color[] col = { GetAvgColor(colors) };
+            return col.ToList();
+        }
+        (float, float)[] ranges = { (1f, 0f), (1f, 0f), (1f, 0f), (1f, 0f) };
+        foreach (Color c in colors)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                float v = c[i];
+                ranges[i] = (Mathf.Min(ranges[i].Item1, v), Mathf.Max(ranges[i].Item2, v));
+            }
+        }
+        float[] totalRanges = { ranges[0].Item2 - ranges[0].Item1, ranges[1].Item2 - ranges[1].Item1, ranges[2].Item2 - ranges[2].Item1, ranges[3].Item2 - ranges[3].Item1 };
+        int dimIndex = totalRanges.ToList().IndexOf(totalRanges.Max());
+
+        List<Color> sortedColors = new List<Color>(colors);
+        sortedColors.Sort((c1, c2) => c1[dimIndex].CompareTo(c2[dimIndex]));
+
+        int midpoint = sortedColors.Count / 2;
+        List<Color> colors1 = sortedColors.Take(midpoint).ToList();
+        List<Color> colors2 = sortedColors.Skip(midpoint).ToList();
+
+        return MedianCutColors(colors1, n - 1).Concat(MedianCutColors(colors2, n - 1)).ToList();
+    }
+
+    List<Color> GenerateKMeansPalette(List<Color> colors, int k, int iterationCount)
+    {
+        List<Vector3> means = new List<Vector3>();
+        List<List<int>> closestIds = new List<List<int>>();
+        for (int i = 0; i < k; i++)
+        {
+            closestIds.Add(new List<int>());
+            means.Add(new Vector3(Random.value, Random.value, Random.value));   //init means
+        }
+
+        for (int i = 0; i < colors.Count; i++)
+        {
+            int id = getClosestID(colors[i], means);
+            closestIds[id].Add(i);
+        }
+
+        for (int i = 0; i < iterationCount; i++)
+        {
+            List<Vector3> newMeans = new List<Vector3>();
+            for (int j = 0; j < k; j++)
+            {
+                List<Color> closeCols = closestIds[j].Select(c => colors[c]).ToList();
+                if (closeCols.Count == 0)
+                {
+                    means[j] = new Vector3(Random.value, Random.value, Random.value);
+                    continue;
+                }
+                Color avgColor = GetAvgColor(closeCols);
+
+                means[j] = new Vector3(avgColor.r, avgColor.g, avgColor.b);
+                closestIds[j] = new List<int>();
+            }
+
+            // means = new List<Vector3>(newMeans);
+            for (int c = 0; c < colors.Count; c++)
+            {
+                int id = getClosestID(colors[c], means);
+                closestIds[id].Add(c);
+            }
+        }
+
+        List<Color> palette = new List<Color>();
+        for (int i = 0; i < k; i++)
+        {
+            palette.Add(new Color(means[i].x, means[i].y, means[i].z));
+        }
+
+        return palette;
+    }
+
+    int getClosestID(Color c, List<Vector3> positions)
+    {
+        float smallest = 999;
+        int smallestID = 0;
+        for (int i = 0; i < positions.Count; i++)
+        {
+            float dist = Vector3.Distance(new Vector3(c.r, c.g, c.b), positions[i]);
+            if (dist < smallest)
+            {
+                smallest = dist;
+                smallestID = i;
+            }
+        }
+        return smallestID;
+    }
+
+    Color GetAvgColor(List<Color> colors)
+    {
+        Color avg = new Color(0, 0, 0, 0);
+        foreach (Color c in colors)
+        {
+            avg += c;
+        }
+        return avg / colors.Count;
+    }
+
+    float[,] MakeThresholdMatrix(float twoN)
+    {
+        if (twoN == 1)
+        {
+            float[,] grid = {
+                {0}
+            };
+            return MultiplyMatrix(grid, 1 / 4f);
+        }
+        float n = twoN / 2;
+        float[,] m = MultiplyMatrix(MakeThresholdMatrix(n), twoN * twoN);
+        float[,] arr = MatrixConcatVertical(MatrixConcatHorizontal(m, AddMatrix(m, 2)), MatrixConcatHorizontal(AddMatrix(m, 3), AddMatrix(m, 1)));
+        return MultiplyMatrix(arr, 1 / (twoN * twoN));
+    }
+
+    void printMatrix(float[,] m)
+    {
+        string str = "";
+        for (int i = 0; i < m.GetLength(0); i++)
+        {
+            for (int j = 0; j < m.GetLength(1); j++)
+            {
+                str += m[i, j].ToString() + " ";
+            }
+            str += "\n";
+        }
+        print(str);
+    }
+
+    float[,] MultiplyMatrix(float[,] matrix, float val)
+    {
+        float[,] result = new float[matrix.GetLength(0), matrix.GetLength(1)];
+
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                result[i, j] = matrix[i, j] * val;
+            }
+        }
+        return result;
+    }
+
+    float[,] AddMatrix(float[,] matrix, float addend)
+    {
+        float[,] result = new float[matrix.GetLength(0), matrix.GetLength(1)];
+
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                result[i, j] = matrix[i, j] + addend;
+            }
+        }
+        return result;
+    }
+
+    float[,] MatrixConcatHorizontal(float[,] m1, float[,] m2)
+    {
+        int rows1 = m1.GetLength(0);
+        int cols1 = m1.GetLength(1);
+
+        int rows2 = m2.GetLength(0);
+        int cols2 = m2.GetLength(1);
+
+        //error handling
+        if (rows1 != rows2)
+        {
+            throw new Exception("matrices must have equal row count, m1: " + rows1.ToString() + ", m2: " + rows2.ToString());
+        }
+
+        float[,] m3 = new float[rows1, cols1 + cols2];
+
+        for (int i = 0; i < rows1; i++)
+        {
+            for (int j = 0; j < cols1; j++)
+            {
+                m3[i, j] = m1[i, j];
+            }
+
+            for (int j = 0; j < cols2; j++)
+            {
+                m3[i, j + cols1] = m2[i, j];
+            }
+        }
+        return m3;
+    }
+
+    float[,] MatrixConcatVertical(float[,] m1, float[,] m2)
+    {
+        int rows1 = m1.GetLength(0);
+        int cols1 = m1.GetLength(1);
+
+        int rows2 = m2.GetLength(0);
+        int cols2 = m2.GetLength(1);
+
+        //error handling
+        if (cols1 != cols2)
+        {
+            throw new Exception("matrices must have equal row count, m1: " + cols1.ToString() + ", m2: " + cols2.ToString());
+        }
+
+        float[,] m3 = new float[rows1 + rows2, cols1];
+
+        for (int i = 0; i < rows1; i++)
+        {
+            for (int j = 0; j < cols1; j++)
+            {
+                m3[i, j] = m1[i, j];
+            }
+        }
+
+        for (int i = 0; i < rows2; i++)
+        {
+            for (int j = 0; j < cols2; j++)
+            {
+                m3[i + rows1, j] = m2[i, j];
+            }
+        }
+
+        return m3;
+    }
+
+    public void InitBuffers()
+    {
+        if (thresholdMatrix == null)
+        {
+            InitThresholdMatrix(8);
+        }
+
+        shader.SetInt("thresholdMatrixSize", thresholdMatrix.GetLength(0));
+        shader.SetInt("paletteSize", palette.Count);
+
+        matrixBuffer = new ComputeBuffer(thresholdMatrix.GetLength(0) * thresholdMatrix.GetLength(0), sizeof(float));
+        matrixBuffer.SetData(thresholdMatrix);
+        shader.SetBuffer(0, "tMatrix", matrixBuffer);
+
+        paletteBuffer = new ComputeBuffer(palette.Count, sizeof(float) * 4);
+        paletteBuffer.SetData(palette);
+        shader.SetBuffer(0, "palette", paletteBuffer);
+    }
+
+    public RenderTexture Dither(Texture2D image)
+    {
+        if (palette == null)
+        {
+            InitPalette(image, 2, 16);
+        }
+
+        result = new RenderTexture(image.width, image.height, 24);  //create Result and blit intput to it
+        result.enableRandomWrite = true;
+        result.Create();
+        Graphics.Blit(image, result);
+        shader.SetTexture(0, "result", result);
+
+
+        shader.SetInt("width", image.width);
+        shader.SetInt("height", image.height);
+
+        shader.Dispatch(0, (int)MathF.Ceiling(image.width / 8f), (int)MathF.Ceiling(image.height / 8f), 1);
+        return result;
+    }
+
+    public void DisposeBuffers()
+    {
+        matrixBuffer?.Dispose();
+        paletteBuffer?.Dispose();
+    }
+}
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,253 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class UIController : MonoBehaviour
+{
+    public Ditherer ditherer;
+
+
+    VisualElement root, autoPaletteSettings, customPaletteSettings, palettePreview;
+    DropdownField paletteType;
+    TextField loadPath, savePath, customColors;
+    IntegerField colorCount, iterationCount, matrixSize, paletteFrame;
+    Button genPaletteButton, runButton;
+    Label errorOutput;
+
+    void OnEnable()
+    {
+        ditherer = GetComponent<Ditherer>();
+
+        root = GetComponent<UIDocument>().rootVisualElement;
+
+        loadPath = root.Q<TextField>("loadPath");
+        savePath = root.Q<TextField>("savePath");
+
+
+        paletteType = root.Q<DropdownField>("paletteType");
+
+        autoPaletteSettings = root.Q<VisualElement>("auto");
+        colorCount = root.Q<IntegerField>("colorCount");
+        iterationCount = root.Q<IntegerField>("iterationCount");
+
+        customPaletteSettings = root.Q<VisualElement>("custom");
+        customColors = root.Q<TextField>("customColors");
+        paletteFrame = root.Q<IntegerField>("paletteFrame");
+
+        matrixSize = root.Q<IntegerField>("matrixSize");
+
+        genPaletteButton = root.Q<Button>("paletteGeneration");
+        palettePreview = root.Q<VisualElement>("palettePreview");
+
+        runButton = root.Q<Button>("runButton");
+
+        paletteType.RegisterValueChangedCallback(evt => PaletteTypeValueChanged(evt.newValue));
+        genPaletteButton.clicked += GeneratePalette;
+
+        runButton.clicked += () => StartCoroutine(RunDitherer());
+
+        savePath.value = Application.persistentDataPath + "/output";
+        if (!Directory.Exists(savePath.value))
+        {
+            Directory.CreateDirectory(savePath.value);
+        }
+
+        errorOutput = root.Q<Label>("errorOutput");
+    }
+
+    void PaletteTypeValueChanged(string selectedValue)
+    {
+        if (selectedValue != "auto")
+        {
+            autoPaletteSettings.style.display = DisplayStyle.None;
+            customPaletteSettings.style.display = DisplayStyle.Flex;
+            return;
+        }
+        autoPaletteSettings.style.display = DisplayStyle.Flex;
+        customPaletteSettings.style.display = DisplayStyle.None;
+        return;
+    }
+
+    void GeneratePalette()
+    {
+        WriteError("generating palette");
+        List<Color> palette = new List<Color>();
+        if (paletteType.value == "auto")
+        {
+            if (File.Exists(loadPath.value))
+            {
+                Texture2D img = LoadPNG(loadPath.value);
+                if (img == null)
+                {
+                    WriteError("Error: couldn't load image");
+                    return;
+                }
+                palette = ditherer.InitPalette(img, colorCount.value, iterationCount.value);
+                WriteError("generated palette for " + loadPath.value);
+            }
+            else if (Directory.Exists(loadPath.value))
+            {
+                string[] images = Directory.GetFiles(loadPath.value, "*.png");
+                if (images.Length == 0)
+                {
+                    WriteError("Error: no images found in folder");
+                    return;
+                }
+                string imgPath = images[(int)Mathf.Clamp(paletteFrame.value, 0, images.Length - 1)];
+                Texture2D img = LoadPNG(imgPath);
+                if (img == null)
+                {
+                    WriteError("Error: couldn't load image");
+                    return;
+                }
+                palette = ditherer.InitPalette(img, colorCount.value, iterationCount.value);
+                WriteError("generated palette for " + imgPath);
+            }
+        }
+        else
+        {
+            List<string> colors = customColors.value.Replace(" ", "").Split(",").ToList();
+            foreach (string hex in colors)
+            {
+                Color color;
+                if (ColorUtility.TryParseHtmlString(hex, out color))
+                {
+                    palette.Add(color);
+                }
+            }
+            ditherer.palette = palette;
+        }
+        palettePreview.Clear();
+        for (int i = 0; i < palette.Count; i++)
+        {
+            VisualElement elem = new VisualElement();
+            elem.AddToClassList("previewColor");
+            elem.style.backgroundColor = new StyleColor(palette[i]);
+            palettePreview.Add(elem);
+        }
+
+    }
+
+    Texture2D LoadPNG(string path)
+    {
+        Texture2D tex = null;
+        byte[] fileData;
+
+        if (File.Exists(path))
+        {
+            fileData = File.ReadAllBytes(path);
+            tex = new Texture2D(2, 2);
+            tex.LoadImage(fileData); //..this will auto-resize the texture dimensions.
+        }
+        return tex;
+    }
+
+    IEnumerator RunDitherer()
+    {
+        WriteError("starting");
+
+        if (!Directory.Exists(savePath.value))
+        {
+            WriteError("Error: save folder not found");
+        }
+
+        if (!IsPowerOfTwo(matrixSize.value))
+        {
+            WriteError("matrixSize is not a power of two");
+            yield break;
+
+        }
+        ditherer.InitThresholdMatrix(matrixSize.value);
+        ditherer.InitBuffers();
+
+        if (File.Exists(loadPath.value))
+        {
+            Dither(loadPath.value);
+        }
+        else if (Directory.Exists(loadPath.value))
+        {
+            string[] images = Directory.GetFiles(loadPath.value, "*.png");
+            if (images.Length == 0)
+            {
+                WriteError("no images found in given folder");
+                ditherer.DisposeBuffers();
+                yield break;
+
+            }
+
+            foreach (string img in images)
+            {
+                yield return null;
+                Dither(img);
+            }
+        }
+        else
+        {
+            WriteError("Error: save path not found");
+            ditherer.DisposeBuffers();
+            yield break;
+        }
+        ditherer.DisposeBuffers();
+        WriteError("done");
+    }
+
+    bool IsPowerOfTwo(int v)
+    {
+        return v != 0 && ((v & (v - 1)) == 0);
+    }
+
+    void Dither(string imgPath)
+    {
+        Texture2D img = LoadPNG(imgPath);
+        if (img == null)
+        {
+            WriteError("Error: image " + imgPath + " wasnt found");
+            return;
+        }
+        if (!Directory.Exists(savePath.value))
+        {
+            WriteError("Error: save path not found");
+            return;
+        }
+        WriteError("currently dithering " + Path.GetFileName(imgPath));
+        RenderTexture result = ditherer.Dither(img);
+
+        savePNG(savePath.value, Path.GetFileName(imgPath), result);
+    }
+
+    void savePNG(string path, string name, RenderTexture rt)
+    {
+        // Create a new texture of the same size as the render texture
+        Texture2D texture2D = new Texture2D(rt.width, rt.height, TextureFormat.RGB24, false);
+
+        // Read the pixels from the render texture
+        RenderTexture.active = rt;
+        texture2D.ReadPixels(new Rect(0, 0, rt.width, rt.height), 0, 0);
+        texture2D.Apply();
+
+        // Reset active render texture
+        RenderTexture.active = null;
+
+        // Convert the texture to a PNG byte array
+        byte[] bytes = texture2D.EncodeToPNG();
+
+        // Define the file path
+        string filePath = path + "/" + name;
+
+        // Write the PNG byte array to a file
+        File.WriteAllBytes(filePath, bytes);
+
+        Debug.Log("Render texture saved to: " + filePath);
+    }
+
+    void WriteError(string msg)
+    {
+        print(msg);
+        errorOutput.text = msg;
+    }
+}
